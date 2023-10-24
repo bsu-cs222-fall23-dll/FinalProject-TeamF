61622b6d0922c6c14af632b655ab5c6201a56f58
Hunter Johns <hunter.johns@bsu.edu>
2023-10-24 12:25:40 -0400

    Remove duplicate Junit dependencies

--- src/test/java/bsu/edu/cs222/connectionTest.java
+++ src/test/java/bsu/edu/cs222/connectionTest.java
@@ -1,7 +1,7 @@
 package bsu.edu.cs222;
 
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class connectionTest extends URLConnection{
     @Test
