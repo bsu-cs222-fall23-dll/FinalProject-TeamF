0983ea557bff6395215e7a7fa77133d72c47c14e
FieryFoxed <122393543+FieryFoxed@users.noreply.github.com>
2023-10-25 15:15:43 -0400

    Commit 6, Finalized the functionality of the gui, added multiple windows when switching to new links, and continued formatting

--- src/main/java/bsu/edu/cs222/Main.java
+++ src/main/java/bsu/edu/cs222/Main.java
@@ -35,13 +35,63 @@ public class Main {
         Iterator<String> iterator = Set.iterator();
         while (iterator.hasNext()) {
             String hyperlink = iterator.next();
+            String link = iterator.next();
+
             JButton button = new JButton(hyperlink);
             Dimension maxButtonSize = new Dimension(100, 20);
             button.setPreferredSize(maxButtonSize);
 
             button.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
+                    continuing(link);
+                    frame.dispose();
+                }
+            });
+            buttonPanel.add(button);
+        }
 
+        JScrollPane scrollPane = new JScrollPane(buttonPanel);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        primary.add(scrollPane);
+        frame.add(primary);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
+    }
+
+    public static void continuing(String link){
+        HyperLinkParser info = new HyperLinkParser();
+
+        JFrame frame = new JFrame("Hyperlinks");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        JPanel primary = new JPanel();
+        primary.setPreferredSize(Toolkit.getDefaultToolkit().getScreenSize());
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(0, 8));
+
+        List<String> next= null;
+        try {
+            next = info.findHyper("https://en.wikipedia.org" + link);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Iterator<String> iterator = next.iterator();
+        while (iterator.hasNext()) {
+            String hyperlink = iterator.next();
+            String nextLink = iterator.next();
+
+            JButton button = new JButton(hyperlink);
+            Dimension maxButtonSize = new Dimension(100, 20);
+            button.setPreferredSize(maxButtonSize);
+
+            button.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    continuing(nextLink);
+                    frame.dispose();
                 }
             });
             buttonPanel.add(button);
