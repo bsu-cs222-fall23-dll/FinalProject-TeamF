f42607f81003963a792a8aefbcb8a49d98e33097
FieryFoxed <122393543+FieryFoxed@users.noreply.github.com>
2023-10-25 11:48:35 -0400

    Commit 5, altered gui layout, and added buttons

--- src/main/java/bsu/edu/cs222/Main.java
+++ src/main/java/bsu/edu/cs222/Main.java
@@ -2,6 +2,8 @@ package bsu.edu.cs222;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
@@ -12,26 +14,41 @@ public class Main {
     public static void main(String[] args) {
         HyperLinkParser info = new HyperLinkParser();
 
-        JPanel primary = new JPanel();
-        primary.setPreferredSize(new Dimension(500, 200));
-        primary.add(new JScrollPane());
-        JTextArea text = new JTextArea();
+        JFrame frame = new JFrame("Hyperlinks");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
 
-        List<String> testSet= null;
+        JPanel primary = new JPanel();
+        primary.setPreferredSize(Toolkit.getDefaultToolkit().getScreenSize());
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(0, 4));
+
+        List<String> Set= null;
         try {
-            testSet = info.findHyper("https://en.wikipedia.org/wiki/Cat");
+            Set = info.findHyper("https://en.wikipedia.org/wiki/Cat");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        Iterator iterator = testSet.iterator();
-        while (iterator.hasNext()){
-            text.append(iterator.next().toString() + "\n");
+        Iterator<String> iterator = Set.iterator();
+        while (iterator.hasNext()) {
+            String hyperlink = iterator.next();
+            JButton button = new JButton(hyperlink);
+            button.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+
+                }
+            });
+            buttonPanel.add(button);
         }
+        buttonPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE));
+        JScrollPane scrollPane = new JScrollPane(buttonPanel);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 
-        JScrollPane append = new JScrollPane(text);
-        primary.add(append);
-
-        JOptionPane.showMessageDialog(null, primary);
+        primary.add(scrollPane);
+        frame.add(primary);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
     }
 }
 
